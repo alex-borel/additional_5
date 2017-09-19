e7ad70ca037e146e6ed930664babfac4007cc79c
Alexander Borel <alex.b.minsk@gmail.com>
2017-09-19 12:49:00 +0300

    brackets/feature: init commit

--- src/index.js
+++ src/index.js
@@ -1,3 +1,21 @@
 module.exports = function check(str, bracketsConfig) {
-  // your solution
+  let res = [];
+  str = str.split('');
+  for (let i = 0; i < str.length; i++) {
+    for (let j = 0; j < bracketsConfig.length; j++) {
+      if (bracketsConfig[j].includes(str[i])) {
+        res.push(true);
+      }
+    }
+  }
+
+  if (str.length > 2 && str.length % 2 !== 0) {
+    return false;
+  }
+  if (str.length === res.length) {
+    return true;
+  } else {
+    return false;
+  }
 }
+
